a9213a842748b6419f80f4ed16ffe9ff41400a7c
Maratuly Temirbolat <snake2009003@mail.ru>
2025-10-07 10:51:24 +0500

    [Backend] Added AbstractBaseModel and refactored Project, Task, and UserTask models to inherit from it.

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin
+# Django modules
+from django.contrib.admin import ModelAdmin, register
 
-# Register your models here.
+# Project modules
+from apps.tasks.models import Task, UserTask, Project
+
+
+@register(Project)
+class ProjectAdmin(ModelAdmin):
+    """
+    Project admin configuration class.
+    """
+
+    ...
+
+
+@register(Task)
+class TaskAdmin(ModelAdmin):
+    """
+    Task admin configuration class.
+    """
+
+    ...
+
+
+@register(UserTask)
+class UserTaskAdmin(ModelAdmin):
+    """
+    UserTask admin configuration class.
+    """
+
+    ...
