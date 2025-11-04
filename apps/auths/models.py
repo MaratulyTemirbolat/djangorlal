21834768325fd6a9b85e124c4efc438554aaae68
Maratuly Temirbolat <snake2009003@mail.ru>
2025-11-04 10:44:56 +0500

    [Backend] Custom User part 2

--- apps/auths/models.py
+++ apps/auths/models.py
@@ -1,4 +1,5 @@
 # Python modules
+from typing import Any
 
 # Django modules
 from django.db.models import (
@@ -6,11 +7,83 @@ from django.db.models import (
     CharField,
     BooleanField,
 )
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 # Project modules
 from apps.abstracts.models import AbstractBaseModel
-from apps.auths.validators import validate_email_domain
+from apps.auths.validators import (
+    validate_email_domain,
+    validate_email_payload_not_in_full_name,
+)
+
+
+class CustomUserManager(BaseUserManager):
+    """Custom User Manager to make database requests."""
+
+    def __obtain_user_instance(
+        self,
+        email: str,
+        full_name: str,
+        password: str,
+        **kwargs: dict[str, Any],
+    ) -> 'CustomUser':
+        """Get user instance."""
+        if not email:
+            raise ValidationError(
+                message="Email field is required", code="email_empty"
+            )
+        if not full_name:
+            raise ValidationError(
+                message="Full name name is required.", code="full_name_empty"
+            )
+
+        new_user: 'CustomUser' = self.model(
+            email=self.normalize_email(email),
+            full_name=full_name,
+            password=password,
+            **kwargs,
+        )
+        return new_user
+
+    def create_user(
+        self,
+        email: str,
+        full_name: str,
+        password: str,
+        **kwargs: dict[str, Any],
+    ) -> 'CustomUser':
+        """Create Custom user. TODO where is this used?"""
+        new_user: 'CustomUser' = self.__obtain_user_instance(
+            email=email,
+            full_name=full_name,
+            password=password,
+            **kwargs,
+        )
+        new_user.set_password(password)
+        new_user.save(using=self._db)
+        return new_user
+
+    def create_superuser(
+        self,
+        email: str,
+        full_name: str,
+        password: str,
+        **kwargs: dict[str, Any],
+    ) -> 'CustomUser':
+        """Create super user. Used by manage.py createsuperuser."""
+        new_user: 'CustomUser' = self.__obtain_user_instance(
+            email=email,
+            full_name=full_name,
+            password=password,
+            is_staff=True,
+            is_superuser=True,
+            **kwargs,
+        )
+        new_user.set_password(password)
+        new_user.save(using=self._db)
+        return new_user
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin, AbstractBaseModel):
@@ -35,6 +108,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin, AbstractBaseModel):
     )
     password = CharField(
         max_length=PASSWORD_MAX_LENGTH,
+        validators=[validate_password],
         verbose_name="Password",
         help_text="User's hash representation of the password",
     )
@@ -53,6 +127,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin, AbstractBaseModel):
 
     REQUIRED_FIELDS = ["full_name"]
     USERNAME_FIELD = "email"
+    objects = CustomUserManager()
 
     class Meta:
         """Meta options for CustomUser model."""
@@ -60,3 +135,11 @@ class CustomUser(AbstractBaseUser, PermissionsMixin, AbstractBaseModel):
         verbose_name = "Custom User"
         verbose_name_plural = "Custom Users"
         ordering = ["-created_at"]
+
+    def clean(self) -> None:
+        """Validate the model instance before saving."""
+        validate_email_payload_not_in_full_name(
+            email=self.email,
+            full_name=self.full_name,
+        )
+        return super().clean()
